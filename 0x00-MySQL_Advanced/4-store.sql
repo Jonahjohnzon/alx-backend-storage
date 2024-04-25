90df13db838ff6a59d3b868a3d92e82165c0eaca
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-25 12:34:28 +0000

    email

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,5 @@
+-- create trigger that decreases quantity of an item after adding a new order
+CREATE TRIGGER buy_trigger
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
