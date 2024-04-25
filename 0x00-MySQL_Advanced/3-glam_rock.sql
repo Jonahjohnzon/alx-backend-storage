d0452f24fe69120b38e4914f24693ce2806d74b7
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-25 12:31:48 +0000

    push

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- All bands with Glam rock, ranked by their longevity.
+-- Lists all bands with Glam rock their main style, ranked by their longevity.
 SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+ORDER BY lifespan DESCi;
