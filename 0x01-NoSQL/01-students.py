5cbe0d22878585aaa9075901d791284d62ca366a
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-30 20:00:57 +0000

    log

---
+++ 0x01-NoSQL/01-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+student
+"""
+
+
+def top_students(mongo_collection):
+    """ student"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
